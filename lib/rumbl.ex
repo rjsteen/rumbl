6de15483ec432c3cef70fe76e144bef23fbb67fc
Richard Steen <richard.steen@activenetwork.com>
2016-08-23 13:46:48 -0500

    yea i should commit more often

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -4,20 +4,13 @@ defmodule Rumbl do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
+    import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Rumbl.Repo, []),
-      # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
-      # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
-      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      supervisor(Rumbl.Repo, []),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
